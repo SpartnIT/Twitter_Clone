8b6088cf274b8b0a5e3898429942c2991a4718a6
SpartnIT <kdavian6@gmail.com>
2023-06-02 00:23:26 -0400

    updated readme file

--- posts/urls.py
+++ posts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('delete/<int:post_id>/',views.delete, name='delete'),
     
     #          <int:post_id>--------(type of data : unique url) specifies the post
-    path('edit/<int:post_id>/',views.edit ),
+    path('edit/<int:post_id>/',views.edit,name='edit' ),
     path('likes/<int:post_id>/',views.likes),
 ]
